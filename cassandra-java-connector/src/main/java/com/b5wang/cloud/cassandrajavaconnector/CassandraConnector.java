aba64b28a5f23e152a2d6b554932af46d78e2f40
neowangbo <neo.wangbo@gmail.com>
2019-08-22 23:10:20 +0800

    create cassandra-java-connector
    create cassandra-java-connector

---
+++ cassandra-java-connector/src/main/java/com/b5wang/cloud/cassandrajavaconnector/CassandraConnector.java
@@ -0,0 +1,25 @@
+package com.b5wang.cloud.cassandrajavaconnector;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+
+public class CassandraConnector {
+
+	public static void main(String[] args) {
+		
+		try (CqlSession session = CqlSession.builder().build()) { // 1. config in application.conf
+			
+			System.out.println("1---");
+			
+			ResultSet rs = session.execute("SELECT username,password,display_name,added_date FROM users");
+			
+			System.out.println("2---");
+			
+			Row row = rs.one();
+			
+			System.out.println("username: " + row.getString("username"));
+		}
+	}
+
+}
