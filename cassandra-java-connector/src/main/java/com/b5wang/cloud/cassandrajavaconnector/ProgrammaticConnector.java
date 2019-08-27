03c52dcb29f3d187d3ddc764e48cfcca3df8dda1
neowangbo <neo.wangbo@gmail.com>
2019-08-27 21:49:33 +0800

    + logback
    + logback

--- cassandra-java-connector/src/main/java/com/b5wang/cloud/cassandrajavaconnector/ProgrammaticConnector.java
+++ cassandra-java-connector/src/main/java/com/b5wang/cloud/cassandrajavaconnector/ProgrammaticConnector.java
@@ -2,12 +2,17 @@ package com.b5wang.cloud.cassandrajavaconnector;
 
 import java.net.InetSocketAddress;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 
 public class ProgrammaticConnector {
 
+	private final static Logger LOG = LoggerFactory.getLogger(ProgrammaticConnector.class);
+	
 	public static void main(String[] args) {
 		CqlSession session = CqlSession.builder()
 				.addContactPoint(new InetSocketAddress("127.0.0.1", 9042))
@@ -15,15 +20,12 @@ public class ProgrammaticConnector {
 			    .withKeyspace("demo")
 			    .build();
 		
-		System.out.println("1---");
-		
 		ResultSet rs = session.execute("SELECT username,password,display_name,added_date FROM users");
-		
-		System.out.println("2---");
-		
 		Row row = rs.one();
 		
-		System.out.println("username: " + row.getString("username"));
+		LOG.info("Result: {}, {}, {}, {}", row.getString("username"),row.getString("password"),row.getString("display_name"),row.getLocalDate("added_date"));
+		
+		session.close();
 	}
 	
 }
