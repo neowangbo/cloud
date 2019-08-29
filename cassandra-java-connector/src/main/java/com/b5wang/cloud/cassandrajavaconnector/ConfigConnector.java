96ed99823036917f76d370de9efe87333f83af73
neowangbo <neo.wangbo@gmail.com>
2019-08-29 23:27:45 +0800

    SimpleStatement
    SimpleStatement

--- cassandra-java-connector/src/main/java/com/b5wang/cloud/cassandrajavaconnector/ConfigConnector.java
+++ cassandra-java-connector/src/main/java/com/b5wang/cloud/cassandrajavaconnector/ConfigConnector.java
@@ -10,15 +10,19 @@ import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 
+/*
+ * 1. application.conf inherits from reference.conf, you only need to redeclare what you override:
+ * 
+ * */
 public class ConfigConnector {
 
-	private final static Logger LOG = LoggerFactory.getLogger(ConfigConnector.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ConfigConnector.class);
+	
+	private static final File CONF = new File("/Users/wangbo/DevHome/projects/GitHub/cloud/cassandra-java-connector/conf-examples/simple-application.conf");
 	
 	public static void main(String[] args) {
 		
-		File conf = new File("/Users/wangbo/DevHome/projects/GitHub/cloud/cassandra-java-connector/conf-examples/simple-application.conf");
-
-		try (CqlSession session = CqlSession.builder().withConfigLoader(DriverConfigLoader.fromFile(conf)).build();) {
+		try (CqlSession session = getCqlSession();) {
 
 			ResultSet rs = session.execute("SELECT username,password,display_name,added_date FROM users");
 			Row row = rs.one();
@@ -26,7 +30,11 @@ public class ConfigConnector {
 			LOG.info("Result: {}, {}, {}, {}", row.getString("username"), row.getString("password"),
 					row.getString("display_name"), row.getLocalDate("added_date"));
 		}
-
+		
+	}
+	
+	public static CqlSession getCqlSession() {
+		return CqlSession.builder().withConfigLoader(DriverConfigLoader.fromFile(CONF)).build();
 	}
 
 }
---
+++ cassandra-java-connector/src/main/java/com/b5wang/cloud/cassandrajavaconnector/SimpleStatementDemo.java
@@ -0,0 +1,129 @@
+package com.b5wang.cloud.cassandrajavaconnector;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.core.cql.SimpleStatementBuilder;
+
+public class SimpleStatementDemo {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SimpleStatementDemo.class);
+	
+	/*
+	 * Present CRUD with SimpleStatement
+	 * */
+	public static void main(String[] args) {
+		String username = "user_1";
+		String password = "123456";
+		String displayName = "Official user";
+		String addedDate = "1987-1-5";
+		
+		//insertUser(username, password, displayName, addedDate);
+		
+		//getUser(username, password);
+		
+		String newDisplayName = "Unformal user name";
+		//updateDisplayName(username, newDisplayName);
+		
+		deleteUser(username, password);
+		
+		selectAll();
+	}
+	
+	
+	private static void insertUser(String username, String password, String displayName, String addedDate) {
+		
+		try (CqlSession session = ConfigConnector.getCqlSession();) {
+			
+			SimpleStatementBuilder statementBuilder = SimpleStatement.builder("INSERT INTO users(username,password,display_name,added_date) VALUES(?,?,?,?)");
+			statementBuilder = statementBuilder.addPositionalValue(username);
+			statementBuilder = statementBuilder.addPositionalValue(password);
+			statementBuilder = statementBuilder.addPositionalValue(displayName);
+			statementBuilder = statementBuilder.addPositionalValue(LocalDate.of(1987, 1, 5));
+			
+			SimpleStatement statement = statementBuilder.build();
+			
+			ResultSet rs = session.execute(statement);
+			for(Row row : rs) {
+				LOG.info("Insert a user: {}, {}, {}, {}", row.getObject("username"), row.getObject("password"), row.getObject("display_name"), row.getObject("added_date"));
+			}
+		}
+	}
+	
+	private static void getUser(String username, String password) {
+		
+		try (CqlSession session = ConfigConnector.getCqlSession();) {
+			
+			SimpleStatementBuilder statementBuilder = SimpleStatement.builder("SELECT username,password,display_name,added_date FROM users WHERE username=? AND password=?");
+			statementBuilder = statementBuilder.addPositionalValue(username);
+			statementBuilder = statementBuilder.addPositionalValue(password);
+			
+			SimpleStatement statement = statementBuilder.build();
+			
+			ResultSet rs = session.execute(statement);
+			for(Row row : rs) {
+				LOG.info("Select a user: {}, {}, {}, {}", row.getObject("username"), row.getObject("password"), row.getObject("display_name"), row.getObject("added_date"));
+			}
+		}
+	}
+	
+	private static void selectAll() {
+		
+		try (CqlSession session = ConfigConnector.getCqlSession();) {
+			
+			SimpleStatementBuilder statementBuilder = SimpleStatement.builder("SELECT username,password,display_name,added_date FROM users");
+			SimpleStatement statement = statementBuilder.build();
+			
+			ResultSet rs = session.execute(statement);
+			for(Row row : rs) {
+				LOG.info("Select a user: {}, {}, {}, {}", row.getObject("username"), row.getObject("password"), row.getObject("display_name"), row.getObject("added_date"));
+			}
+		}
+	}
+	
+	/**
+	 * Any of primary keys is not able to be changed!!!
+	 * */
+	private static void updateDisplayName(String username, String displayName) {
+		
+		try (CqlSession session = ConfigConnector.getCqlSession();) {
+			
+			SimpleStatementBuilder statementBuilder = SimpleStatement.builder("UPDATE users SET display_name=:displayName WHERE username=:username");
+			statementBuilder = statementBuilder.addNamedValue("username", username);
+			statementBuilder = statementBuilder.addNamedValue("displayName", displayName);
+			
+			SimpleStatement statement = statementBuilder.build();
+			
+			ResultSet rs = session.execute(statement);
+			for(Row row : rs) {
+				LOG.info("Update a user: {}, {}, {}, {}", row.getObject("username"), row.getObject("password"), row.getObject("display_name"), row.getObject("added_date"));
+			}
+		}
+	}
+	
+	/**
+	 * must have partition key
+	 * */
+	private static void deleteUser(String username, String password) {
+		try (CqlSession session = ConfigConnector.getCqlSession();) {
+			
+			SimpleStatementBuilder statementBuilder = SimpleStatement.builder("DELETE FROM users WHERE username=? AND password=?");
+			statementBuilder = statementBuilder.addPositionalValue(username);
+			statementBuilder = statementBuilder.addPositionalValue(password);
+			
+			SimpleStatement statement = statementBuilder.build();
+			
+			ResultSet rs = session.execute(statement);
+			for(Row row : rs) {
+				LOG.info("Delete a user: {}, {}, {}, {}", row.getObject("username"), row.getObject("password"), row.getObject("display_name"), row.getObject("added_date"));
+			}
+		}
+	}
+	
+}
